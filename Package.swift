f0adf96e89c8e43bac46c51516c1d9a519256888
Umberto Raimondi <me@uraimo.com>
2016-02-22 18:17:30 +0100

    Added dependency from SwiftyGPIO

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "PCD8544.swift"
+    name: "PCD8544.swift",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git",
+                 majorVersion: 1)
+    ]
 )
