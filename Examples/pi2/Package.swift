ee9ece8b642277039d5ae05a1670dc86a1bc68c4
Umberto Raimondi <me@uraimo.com>
2018-06-05 21:26:24 +0200

    SwiftyGPIO to 1.x

--- Examples/pi2/Package.swift
+++ Examples/pi2/Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Test5110",
     dependencies: [
-        .Package(url: "https://github.com/uraimo/5110LCD_PCD8544.swift.git", majorVersion: 2),
+        .Package(url: "https://github.com/uraimo/5110LCD_PCD8544.swift.git", majorVersion: 3),
     ]
 )
