ae2ab9adb0013717a06e623e9c424069c86d2816
Radagan <radagan@gmail.com>
2019-03-28 15:07:40 +1000

    Updated package format and updated to 4.2. There were a few errors related to truncatedbitpattern that were fixed.

--- Package.swift
+++ Package.swift
@@ -1,9 +1,23 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "PCD8544",
+    products: [
+        .library(
+            name: "PCD8544",
+            targets: ["PCD8544"]),
+    ],
     dependencies: [
-        .Package(url: "https://github.com/uraimo/SwiftyGPIO.git",
-                 majorVersion: 1)
+        .package(url: "https://github.com/uraimo/SwiftyGPIO.git", from: "1.1.0"),
+    ],
+    targets: [
+        .target(
+            name: "PCD8544",
+            dependencies: ["SwiftyGPIO"],
+            path: ".",
+            sources: ["Sources"])
     ]
 )
